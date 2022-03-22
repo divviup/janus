45db47f064444839c7f1e85caf7b69615a71d23f
Brandon Pitman <brandon@letsencrypt.org>
2022-03-21 17:07:54 -0700

    /upload endpoint: store reports to a Datastore.
    This replaces the previous temporary solution of storing them in-memory
    only. This commit also includes some refactoring of the ephemeral
    datastore test functionality to make it easier to use & ease later
    refactorings.

--- janus_server/src/datastore.rs
+++ janus_server/src/datastore.rs
@@ -1,7 +1,7 @@
 //! Janus datastore (durable storage) implementation.
 
 use crate::message::{Extension, HpkeCiphertext, Nonce, Report, TaskId, Time};
-use prio::codec::{decode_u16_items, encode_u16_items, CodecError, Decode};
+use prio::codec::{decode_u16_items, encode_u16_items, CodecError, Decode, Encode};
 use std::{future::Future, io::Cursor, pin::Pin};
 use tokio_postgres::{error::SqlState, IsolationLevel, Row};
 
@@ -30,7 +30,8 @@ impl Datastore {
     /// "finalized" until the transaction is committed, i.e. after `run_tx` is run to completion.
     pub async fn run_tx<F, T>(&self, f: F) -> Result<T, Error>
     where
-        for<'a> F: Fn(&'a Transaction) -> Pin<Box<dyn Future<Output = Result<T, Error>> + 'a>>,
+        for<'a> F:
+            Fn(&'a Transaction) -> Pin<Box<dyn Future<Output = Result<T, Error>> + Send + 'a>>,
     {
         loop {
             let rslt = self.run_tx_once(&f).await;
@@ -45,7 +46,8 @@ impl Datastore {
 
     async fn run_tx_once<F, T>(&self, f: &F) -> Result<T, Error>
     where
-        for<'a> F: Fn(&'a Transaction) -> Pin<Box<dyn Future<Output = Result<T, Error>> + 'a>>,
+        for<'a> F:
+            Fn(&'a Transaction) -> Pin<Box<dyn Future<Output = Result<T, Error>> + Send + 'a>>,
     {
         // Open transaction.
         let mut client = self.pool.get().await?;
@@ -75,7 +77,7 @@ impl Transaction<'_> {
     // TODO(brandon): implement basic getters/putters for all types
 
     #[cfg(test)]
-    async fn put_task(&self, task_id: TaskId) -> Result<(), Error> {
+    pub async fn put_task(&self, task_id: TaskId) -> Result<(), Error> {
         let stmt = self
             .tx
             .prepare_cached(
@@ -120,6 +122,50 @@ impl Transaction<'_> {
                 time: nonce_time,
                 rand: nonce_rand as u64,
             },
+            extensions,
+            encrypted_input_shares: input_shares,
+        })
+    }
+
+    pub async fn get_client_report_by_task_id_and_nonce(
+        &self,
+        task_id: TaskId,
+        nonce: Nonce,
+    ) -> Result<Report, Error> {
+        let nonce_time = nonce.time.as_naive_date_time();
+        let nonce_rand = nonce.rand as i64;
+
+        let stmt = self
+            .tx
+            .prepare_cached(
+                "SELECT extensions, input_shares FROM client_reports
+            WHERE task_id = $1 AND nonce_time = $2 AND nonce_rand = $3",
+            )
+            .await?;
+        let row = single_row(
+            self.tx
+                .query(
+                    &stmt,
+                    &[
+                        /* task_id */ &task_id.get_encoded(),
+                        /* nonce_time */ &nonce_time,
+                        /* nonce_rand */ &nonce_rand,
+                    ],
+                )
+                .await?,
+        )?;
+
+        let encoded_extensions: Vec<u8> = row.get("extensions");
+        let extensions: Vec<Extension> =
+            decode_u16_items(&(), &mut Cursor::new(&encoded_extensions))?;
+
+        let encoded_input_shares: Vec<u8> = row.get("input_shares");
+        let input_shares: Vec<HpkeCiphertext> =
+            decode_u16_items(&(), &mut Cursor::new(&encoded_input_shares))?;
+
+        Ok(Report {
+            task_id,
+            nonce,
             extensions,
             encrypted_input_shares: input_shares,
         })
@@ -208,25 +254,35 @@ impl Error {
     }
 }
 
-#[cfg(test)]
-pub(crate) mod test_util {
+// debug_assertions allows this module to be used in integration tests.
+// XXX: if the debug_assertions strategy doesn't make it through code review, move things back to dev-dependencies
+#[cfg(any(test, debug_assertions))]
+pub mod test_util {
     use super::*;
     use deadpool_postgres::{Manager, Pool};
+    use lazy_static::lazy_static;
     use std::str::{self, FromStr};
-    use testcontainers::{images::postgres::Postgres, Container, Docker};
+    use testcontainers::{clients::Cli, images::postgres::Postgres, Container, Docker};
     use tokio_postgres::{Config, NoTls};
 
     const SCHEMA: &str = include_str!("../../db/schema.sql");
+
+    // TODO(brandon): use podman instead of docker for container management once testcontainers supports it
+    lazy_static! {
+        static ref DOCKER: Cli = Cli::default();
+    }
+
+    /// DbHandle represents a handle to a running (ephemeral) database. Dropping this value causes
+    /// the database to be shut down & cleaned up.
+    pub struct DbHandle(Container<'static, Cli, Postgres>);
 
     /// ephemeral_datastore creates a new Datastore instance backed by an ephemeral database which
     /// has the Janus schema applied but is otherwise empty.
     ///
     /// Dropping the second return value causes the database to be shut down & cleaned up.
-    pub(crate) async fn ephemeral_datastore<D: Docker>(
-        container_client: &D,
-    ) -> (Datastore, Container<'_, D, Postgres>) {
+    pub async fn ephemeral_datastore() -> (Datastore, DbHandle) {
         // Start an instance of Postgres running in a container.
-        let db_container = container_client.run(Postgres::default().with_version(14));
+        let db_container = DOCKER.run(Postgres::default().with_version(14));
 
         // Create a connection pool whose clients will talk to our newly-running instance of Postgres.
         const POSTGRES_DEFAULT_PORT: u16 = 5432;
@@ -241,25 +297,21 @@ pub(crate) mod test_util {
         // Connect to the database & run our schema.
         let client = pool.get().await.unwrap();
         client.batch_execute(SCHEMA).await.unwrap();
-        (Datastore::new(pool), db_container)
+        (Datastore::new(pool), DbHandle(db_container))
     }
 }
 
 #[cfg(test)]
 mod tests {
-    // TODO(brandon): use podman instead of docker for container management once testcontainers supports this
-
     use super::*;
     use crate::datastore::test_util::ephemeral_datastore;
     use crate::message::{ExtensionType, HpkeConfigId};
     use crate::trace::test_util::install_trace_subscriber;
-    use testcontainers::clients;
 
     #[tokio::test]
     async fn roundtrip_report() {
         install_trace_subscriber();
-        let docker = clients::Cli::default();
-        let (ds, _db_container) = ephemeral_datastore(&docker).await;
+        let (ds, _db_handle) = ephemeral_datastore().await;
 
         let task_id = TaskId([
             0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23,
@@ -317,11 +369,99 @@ mod tests {
     #[tokio::test]
     async fn report_not_found() {
         install_trace_subscriber();
-        let docker = clients::Cli::default();
-        let (ds, _db_container) = ephemeral_datastore(&docker).await;
+        let (ds, _db_handle) = ephemeral_datastore().await;
 
         let rslt = ds
             .run_tx(|tx| Box::pin(async move { tx.get_client_report(12345).await }))
+            .await;
+
+        assert_matches::assert_matches!(rslt, Err(Error::NotFound));
+    }
+
+    #[tokio::test]
+    async fn roundtrip_report_by_task_id_and_nonce() {
+        install_trace_subscriber();
+        let (ds, _db_handle) = ephemeral_datastore().await;
+
+        let task_id = TaskId([
+            0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23,
+            24, 25, 26, 27, 28, 29, 30, 31,
+        ]);
+        let nonce = Nonce {
+            time: Time(12345),
+            rand: 54321,
+        };
+        let report = Report {
+            task_id,
+            nonce,
+            extensions: vec![
+                Extension {
+                    extension_type: ExtensionType::Tbd,
+                    extension_data: Vec::from("extension_data_0"),
+                },
+                Extension {
+                    extension_type: ExtensionType::Tbd,
+                    extension_data: Vec::from("extension_data_1"),
+                },
+            ],
+            encrypted_input_shares: vec![
+                HpkeCiphertext {
+                    config_id: HpkeConfigId(12),
+                    encapsulated_context: Vec::from("encapsulated_context_0"),
+                    payload: Vec::from("payload_0"),
+                },
+                HpkeCiphertext {
+                    config_id: HpkeConfigId(13),
+                    encapsulated_context: Vec::from("encapsulated_context_1"),
+                    payload: Vec::from("payload_1"),
+                },
+            ],
+        };
+
+        ds.run_tx(|tx| {
+            let report = report.clone();
+            Box::pin(async move {
+                tx.put_task(task_id).await?;
+                tx.put_client_report(&report).await
+            })
+        })
+        .await
+        .unwrap();
+
+        let retrieved_report = ds
+            .run_tx(|tx| {
+                Box::pin(async move {
+                    tx.get_client_report_by_task_id_and_nonce(task_id, nonce)
+                        .await
+                })
+            })
+            .await
+            .unwrap();
+
+        assert_eq!(report, retrieved_report);
+    }
+
+    #[tokio::test]
+    async fn report_not_found_by_task_id_and_nonce() {
+        install_trace_subscriber();
+        let (ds, _db_handle) = ephemeral_datastore().await;
+
+        let task_id = TaskId([
+            0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23,
+            24, 25, 26, 27, 28, 29, 30, 31,
+        ]);
+        let nonce = Nonce {
+            time: Time(12345),
+            rand: 54321,
+        };
+
+        let rslt = ds
+            .run_tx(|tx| {
+                Box::pin(async move {
+                    tx.get_client_report_by_task_id_and_nonce(task_id, nonce)
+                        .await
+                })
+            })
             .await;
 
         assert_matches::assert_matches!(rslt, Err(Error::NotFound));
