ccc52892b9f2685a0f281a69c1bbf9dce901cd15
David Cook <dcook@letsencrypt.org>
2022-04-11 16:33:39 -0500

    Get database password from env. var. if not in URL

--- janus_server/src/bin/aggregator.rs
+++ janus_server/src/bin/aggregator.rs
@@ -90,13 +90,18 @@ async fn main() -> Result<()> {
     let vdaf = Prio3Aes128Count::new(2).unwrap();
     let verify_param = vdaf.setup().unwrap().1.first().unwrap().clone();
 
-    let database_config = tokio_postgres::Config::from_str(config.database.url.as_str())
+    let mut database_config = tokio_postgres::Config::from_str(config.database.url.as_str())
         .with_context(|| {
             format!(
                 "failed to parse database connect string: {:?}",
                 config.database.url
             )
         })?;
+    if database_config.get_password().is_none() {
+        if let Ok(password) = std::env::var("PGPASSWORD") {
+            database_config.password(password);
+        }
+    }
     let conn_mgr = Manager::new(database_config, NoTls);
     let pool = Pool::builder(conn_mgr)
         .build()
