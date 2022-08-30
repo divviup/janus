d971ce034e81ff4f99d879097905606b775fae2e
Brandon Pitman <brandon@letsencrypt.org>
2022-08-29 19:08:12 -0700

    Add support for use of prebuilt test Daphne container image. (#435)
    This is controlled by a new compile-time DAPHNE_INTEROP_CONTAINER
    environment variable (matching the JANUS_INTEROP_CONTAINER variable used
    to control building of the Janus interop test containers). See the
    updates to README.md for more details.
    
    Also:
    * Update Daphne to the latest commit. This is important as it picks up
      https://github.com/cloudflare/daphne/pull/99 which de-flakes
      Janus/Daphne integration tests again after they were accidentally
      re-flaked by switching to DAP_DEPLOYMENT=prod.
    * Add ability to skip building the Daphne container, and do so for our
      CI lints.

--- monolithic_integration_test/README.md
+++ monolithic_integration_test/README.md
@@ -7,7 +7,7 @@ compiled version of Daphne inside a container. The test container is built by th
 script based on the test Dockerfile in the Daphne repository; the test container is included in
 necessary test binaries which know how to load the image into Docker themselves.
 
-Daphne is compiled from commit [`6228556c7b87a7fe85e414a3186a2511407896f0`](
+Daphne is compiled from commit [`e1b503eb2aefadfe2717abb0a359892848175534`](
 https://github.com/cloudflare/daphne/commit/6228556c7b87a7fe85e414a3186a2511407896f0).
 
 ### Running Daphne integration tests
@@ -23,3 +23,20 @@ To update the version of Daphne in use:
 1. Update `Cargo.toml` in this directory to reference the new commit of Daphne.
 1. Update `build.rs` in this directory to reference the new commit of Daphne.
 1. Update this README to note the new commit of Daphne.
+
+### Using a prebuilt Daphne
+
+It is possible to instruct Janus' tests to use a prebuilt Docker image for the Janus/Daphne
+integration tests, rather than building an image as part of the build process. To do this, set the
+`DAPHNE_INTEROP_CONTAINER` environment variable to `prebuilt=${IMAGE_NAME}:${IMAGE_TAG}`. For
+example, to use an image named `test_daphne` with the `latest` tag, set
+`DAPHNE_INTEROP_CONTAINER=prebuilt=test_daphne:latest`.
+
+To build a new Daphne image suitable for use as a prebuilt image, clone the [Daphne repository](
+https://github.com/cloudflare/daphne), check out the commit of interest, and then run a command like
+`docker build --file=daphne_worker_test/docker/miniflare.Dockerfile --tag=test_daphne:$(git rev-parse HEAD | head -c8) .`.
+This command will generate an image named `test_daphne` tagged with a prefix of the git commit hash
+used to generate it.
+
+Note that even when using prebuilt container images, it is still important to match the image
+version with the version of Daphne referenced by this package in `Cargo.toml`.
--- monolithic_integration_test/build.rs
+++ monolithic_integration_test/build.rs
@@ -2,75 +2,163 @@ fn main() {
     #[cfg(feature = "daphne")]
     {
         use build_script_utils::save_zstd_compressed_docker_image;
+        use serde_json::json;
         use std::{env, fs::File, process::Command};
         use tempfile::tempdir;
 
         // This build script is self-contained, so we only need to rebuild if the build script
-        // itself changes.
+        // itself or one of its inputs changes.
         println!("cargo:rerun-if-changed=build.rs");
+        println!("cargo:rerun-if-env-changed=DAPHNE_INTEROP_CONTAINER");
 
-        // Check out Daphne repository at a fixed hash, then build & save off a container image for
-        // the test Daphne instance.
-        const DAPHNE_COMMIT_HASH: &str = "6228556c7b87a7fe85e414a3186a2511407896f0";
-        let daphne_checkout_dir = tempdir().unwrap();
-        let clone_output = Command::new("git")
-            .args(["clone", "-n", "https://github.com/cloudflare/daphne", "."])
-            .current_dir(&daphne_checkout_dir)
-            .output()
-            .expect("Failed to execute `git clone` for Daphne repository");
-        assert!(
-            clone_output.status.success(),
-            "Git clone of Daphne repository failed:\n{}",
-            String::from_utf8_lossy(&clone_output.stderr)
-        );
-        let checkout_output = Command::new("git")
-            .args(["checkout", DAPHNE_COMMIT_HASH])
-            .current_dir(&daphne_checkout_dir)
-            .output()
-            .expect("Failed to execute `git checkout` for Daphne repository");
-        assert!(
-            checkout_output.status.success(),
-            "Git checkout of Daphne repository failed:\n{}",
-            String::from_utf8_lossy(&checkout_output.stderr)
-        );
+        let container_strategy = env::var("DAPHNE_INTEROP_CONTAINER")
+            .ok()
+            .unwrap_or_else(|| "build".to_string());
 
-        // Note: `docker build` has an `--output` flag which writes the output to somewhere, which
-        // may be a tarfile. But `docker build --output` only exports the image filesystem, and not
-        // any other image metadata (such as exposed ports, the entrypoint, etc), so we can't easily
-        // use it.
-        let build_output = Command::new("docker")
-            .args([
-                "build",
-                "--file=daphne_worker_test/docker/miniflare.Dockerfile",
-                "--quiet",
-                ".",
-            ])
-            .current_dir(&daphne_checkout_dir)
-            .output()
-            .expect("Failed to execute `docker build` for test Daphne");
-        assert!(
-            build_output.status.success(),
-            "Docker build of test Daphne failed:\n{}",
-            String::from_utf8_lossy(&build_output.stderr)
-        );
-        let image_id = String::from_utf8(build_output.stdout).unwrap();
-        let image_id = image_id.trim();
+        if container_strategy == "build" {
+            // Check out Daphne repository at a fixed hash, then build & save off a container image for
+            // the test Daphne instance.
+            const DAPHNE_COMMIT_HASH: &str = "e1b503eb2aefadfe2717abb0a359892848175534";
+            let daphne_checkout_dir = tempdir().unwrap();
+            let clone_output = Command::new("git")
+                .args(["clone", "-n", "https://github.com/cloudflare/daphne", "."])
+                .current_dir(&daphne_checkout_dir)
+                .output()
+                .expect("Failed to execute `git clone` for Daphne repository");
+            assert!(
+                clone_output.status.success(),
+                "Git clone of Daphne repository failed:\n{}",
+                String::from_utf8_lossy(&clone_output.stderr)
+            );
+            let checkout_output = Command::new("git")
+                .args(["checkout", DAPHNE_COMMIT_HASH])
+                .current_dir(&daphne_checkout_dir)
+                .output()
+                .expect("Failed to execute `git checkout` for Daphne repository");
+            assert!(
+                checkout_output.status.success(),
+                "Git checkout of Daphne repository failed:\n{}",
+                String::from_utf8_lossy(&checkout_output.stderr)
+            );
 
-        let image_file = File::create(format!(
-            "{}/test_daphne.tar.zst",
-            env::var("OUT_DIR").unwrap()
-        ))
-        .expect("Couldn't create test Daphne image file");
-        save_zstd_compressed_docker_image(image_id, &image_file);
-        image_file
-            .sync_all()
-            .expect("Couldn't write compressed image file");
-        drop(image_file);
+            // Note: `docker build` has an `--output` flag which writes the output to somewhere, which
+            // may be a tarfile. But `docker build --output` only exports the image filesystem, and not
+            // any other image metadata (such as exposed ports, the entrypoint, etc), so we can't easily
+            // use it.
+            let build_output = Command::new("docker")
+                .args([
+                    "build",
+                    "--file=daphne_worker_test/docker/miniflare.Dockerfile",
+                    "--quiet",
+                    ".",
+                ])
+                .current_dir(&daphne_checkout_dir)
+                .output()
+                .expect("Failed to execute `docker build` for test Daphne");
+            assert!(
+                build_output.status.success(),
+                "Docker build of test Daphne failed:\n{}",
+                String::from_utf8_lossy(&build_output.stderr)
+            );
+            let image_id = String::from_utf8(build_output.stdout).unwrap();
+            let image_id = image_id.trim();
 
-        // Make a best-effort attempt to clean up after ourselves.
-        Command::new("docker")
-            .args(["rmi", image_id])
-            .status()
-            .expect("Failed to execute `docker rmi` for test Daphne");
+            let image_file = File::create(format!(
+                "{}/test_daphne.tar.zst",
+                env::var("OUT_DIR").unwrap()
+            ))
+            .expect("Couldn't create test Daphne image file");
+            save_zstd_compressed_docker_image(image_id, &image_file);
+            image_file
+                .sync_all()
+                .expect("Couldn't write compressed image file");
+            drop(image_file);
+
+            // Write metadata file instructing runtime to reference the container we built.
+            let metadata_file = File::create(format!(
+                "{}/test_daphne.metadata",
+                env::var("OUT_DIR").unwrap()
+            ))
+            .expect("Couldn't create test Daphne metadata file");
+            serde_json::to_writer(&metadata_file, &json!({"strategy": "build"}))
+                .expect("Couldn't write metadata file");
+            metadata_file
+                .sync_all()
+                .expect("Couldn't write metadata file");
+            drop(metadata_file);
+
+            // Make a best-effort attempt to clean up after ourselves.
+            Command::new("docker")
+                .args(["image", "remove", image_id])
+                .status()
+                .expect("Failed to execute `docker image remove` for test Daphne");
+        } else if container_strategy.starts_with("prebuilt=") {
+            let (image_name, image_tag) = container_strategy
+                .strip_prefix("prebuilt=")
+                .unwrap()
+                .split_once(':')
+                .unwrap();
+
+            // Write empty image file (required for compilation to succeed).
+            let image_file = File::create(format!(
+                "{}/test_daphne.tar.zst",
+                env::var("OUT_DIR").unwrap()
+            ))
+            .expect("Couldn't create test Daphne image file");
+            image_file
+                .sync_all()
+                .expect("Couldn't write compressed image file");
+            drop(image_file);
+
+            // Write metadata file instructing runtime to reference our prebuilt container.
+            let metadata_file = File::create(format!(
+                "{}/test_daphne.metadata",
+                env::var("OUT_DIR").unwrap()
+            ))
+            .expect("Couldn't create test Daphne metadata file");
+            serde_json::to_writer(
+                &metadata_file,
+                &json!({
+                    "strategy": "prebuilt",
+                    "image_name": image_name,
+                    "image_tag": image_tag,
+                }),
+            )
+            .expect("Couldn't write metadata file");
+            metadata_file
+                .sync_all()
+                .expect("Couldn't write metadata file");
+            drop(metadata_file);
+        } else if container_strategy == "skip" {
+            // The "skip" strategy causes us to skip building a container at all. Tests which
+            // depend on having Daphne interop test images available will fail.
+
+            // Write empty image file (required for compilation to succeed).
+            let image_file = File::create(format!(
+                "{}/test_daphne.tar.zst",
+                env::var("OUT_DIR").unwrap()
+            ))
+            .expect("Couldn't create test Daphne image file");
+            image_file
+                .sync_all()
+                .expect("Couldn't write compressed image file");
+            drop(image_file);
+
+            // Write metadata file instructing runtime that we skipped the container build (which
+            // will cause anyone attempting to instantiate the container to panic).
+            let metadata_file = File::create(format!(
+                "{}/test_daphne.metadata",
+                env::var("OUT_DIR").unwrap()
+            ))
+            .expect("Couldn't create test Daphne metadata file");
+            serde_json::to_writer(&metadata_file, &json!({"strategy": "skip"}))
+                .expect("Couldn't write metadata file");
+            metadata_file
+                .sync_all()
+                .expect("Couldn't write metadata file");
+            drop(metadata_file);
+        } else {
+            panic!("Unexpected DAPHNE_INTEROP_CONTAINER value {container_strategy:?} (valid values are \"build\", \"prebuilt=image_name:image_tag\", & \"skip\")")
+        }
     }
 }
