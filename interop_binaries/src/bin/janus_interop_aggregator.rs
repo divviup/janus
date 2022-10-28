5fd3cb91380b0ddd81eac04f20f73767fa9f60a5
David Cook <dcook@divviup.org>
2022-10-28 14:12:20 -0500

    Split janus_interop_aggregator into subprocesses

--- interop_binaries/src/bin/janus_interop_aggregator.rs
+++ interop_binaries/src/bin/janus_interop_aggregator.rs
@@ -1,36 +1,24 @@
-use anyhow::{anyhow, Context};
+use anyhow::Context;
 use base64::URL_SAFE_NO_PAD;
-use clap::{value_parser, Arg, Command};
+use clap::Parser;
 use janus_aggregator::{
-    aggregator::{
-        aggregate_share::CollectJobDriver, aggregation_job_creator::AggregationJobCreator,
-        aggregation_job_driver::AggregationJobDriver, aggregator_filter,
-    },
-    binary_utils::{database_pool, job_driver::JobDriver},
-    config::DbConfig,
-    datastore::{Crypter, Datastore},
+    aggregator::aggregator_filter,
+    binary_utils::{janus_main, BinaryOptions, CommonBinaryOptions},
+    config::{BinaryConfig, CommonConfig},
+    datastore::Datastore,
     task::Task,
     SecretBytes,
 };
-use janus_core::{task::AuthenticationToken, time::RealClock, TokioRuntime};
+use janus_core::{task::AuthenticationToken, time::RealClock};
 use janus_interop_binaries::{
-    install_tracing_subscriber,
     status::{ERROR, SUCCESS},
     AddTaskResponse, AggregatorAddTaskRequest, AggregatorRole, HpkeConfigRegistry,
 };
 use janus_messages::{BatchId, Duration, HpkeConfig, TaskId, Time};
-use opentelemetry::global::meter;
 use prio::codec::Decode;
-use rand::random;
-use ring::aead::{LessSafeKey, UnboundKey, AES_128_GCM};
 use serde::{Deserialize, Serialize};
-use std::{
-    net::{Ipv4Addr, SocketAddr},
-    sync::Arc,
-    time::Duration as StdDuration,
-};
+use std::{net::SocketAddr, sync::Arc};
 use tokio::sync::Mutex;
-use url::Url;
 use warp::{hyper::StatusCode, reply::Response, Filter, Reply};
 
 #[derive(Debug, Serialize)]
@@ -240,146 +228,77 @@ fn make_filter(
         .unify())
 }
 
-fn app() -> clap::Command {
-    Command::new("Janus interoperation test aggregator")
-        .arg(
-            Arg::new("port")
-                .long("port")
-                .short('p')
-                .default_value("8080")
-                .value_parser(value_parser!(u16))
-                .help("Port number to listen on."),
-        )
-        .arg(
-            Arg::new("postgres-url")
-                .long("postgres-url")
-                .default_value("postgres://postgres@127.0.0.1:5432/postgres")
-                .value_parser(value_parser!(Url))
-                .help("PostgreSQL database connection URL."),
-        )
-        .arg(
-            Arg::new("dap-serving-prefix")
-                .long("dap-serving-prefix")
-                .default_value("/")
-                .help("Path prefix, e.g. `/dap/`, to serve DAP from"),
-        )
+#[derive(Debug, Parser)]
+#[clap(
+    name = "janus_interop_aggregator",
+    about = "Janus interoperation test aggregator",
+    rename_all = "kebab-case",
+    version = env!("CARGO_PKG_VERSION")
+)]
+struct Options {
+    #[clap(flatten)]
+    common: CommonBinaryOptions,
+}
+
+impl BinaryOptions for Options {
+    fn common_options(&self) -> &CommonBinaryOptions {
+        {
+            &self.common
+        }
+    }
+}
+
+fn default_dap_serving_prefix() -> String {
+    "/".to_string()
+}
+
+#[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
+struct Config {
+    #[serde(flatten)]
+    common_config: CommonConfig,
+
+    /// Address on which this server should listen for connections and serve its
+    /// API endpoints.
+    listen_address: SocketAddr,
+
+    /// Path prefix, e.g. `/dap/`, to serve DAP from.
+    #[serde(default = "default_dap_serving_prefix")]
+    dap_serving_prefix: String,
+}
+
+impl BinaryConfig for Config {
+    fn common_config(&self) -> &CommonConfig {
+        &self.common_config
+    }
+
+    fn common_config_mut(&mut self) -> &mut CommonConfig {
+        &mut self.common_config
+    }
 }
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    install_tracing_subscriber()?;
-    let matches = app().get_matches();
-    let http_port = matches
-        .try_get_one::<u16>("port")?
-        .ok_or_else(|| anyhow!("port argument missing"))?;
-    let postgres_url = matches
-        .try_get_one::<Url>("postgres-url")?
-        .ok_or_else(|| anyhow!("postgres-url argument missing"))?;
-    let dap_serving_prefix = matches
-        .get_one("dap-serving-prefix")
-        .map(Clone::clone)
-        .unwrap_or_else(|| "/".to_string());
+    janus_main::<_, Options, Config, _, _>(RealClock::default(), |ctx| async move {
+        let datastore = Arc::new(ctx.datastore);
 
-    // Make an ephemeral datastore key.
-    let key_bytes: [u8; 16] = random();
-    let datastore_key = LessSafeKey::new(UnboundKey::new(&AES_128_GCM, &key_bytes).unwrap());
-    let crypter = Crypter::new(Vec::from([datastore_key]));
+        // Run an HTTP server with both the DAP aggregator endpoints and the interoperation test
+        // endpoints.
+        let filter = make_filter(Arc::clone(&datastore), ctx.config.dap_serving_prefix)?;
+        let server = warp::serve(filter);
+        server.bind(ctx.config.listen_address).await;
 
-    // Connect to database, apply schema, and set up datastore.
-    let db_config = DbConfig {
-        url: postgres_url.clone(),
-        connection_pool_timeouts_secs: 30,
-    };
-    let pool = database_pool(&db_config, None).await?;
-    let clock = RealClock::default();
-    let client = pool.get().await?;
-    client
-        .batch_execute(include_str!("../../../db/schema.sql"))
-        .await?;
-    // Return the database connection we used to deploy the schema back to the pool, so it can be
-    // reused.
-    drop(client);
-    let datastore = Arc::new(Datastore::new(pool, crypter, clock));
-
-    // Run an HTTP server with both the DAP aggregator endpoints and the interoperation test
-    // endpoints.
-    let filter = make_filter(Arc::clone(&datastore), dap_serving_prefix)?;
-    let server = warp::serve(filter);
-    let aggregator_future = server.bind(SocketAddr::from((Ipv4Addr::UNSPECIFIED, *http_port)));
-
-    // Run the aggregation job creator.
-    let pool = database_pool(&db_config, None).await?;
-    let datastore_key = LessSafeKey::new(UnboundKey::new(&AES_128_GCM, &key_bytes).unwrap());
-    let crypter = Crypter::new(Vec::from([datastore_key]));
-    let aggregation_job_creator = Arc::new(AggregationJobCreator::new(
-        Datastore::new(pool, crypter, clock),
-        clock,
-        StdDuration::from_secs(2),
-        StdDuration::from_secs(1),
-        1,
-        100,
-    ));
-    let aggregation_job_creator_future = aggregation_job_creator.run();
-
-    // Run the aggregation job driver.
-    let aggregation_job_driver_meter = meter("aggregation_job_driver");
-    let aggregation_job_driver = Arc::new(AggregationJobDriver::new(
-        reqwest::Client::new(),
-        &aggregation_job_driver_meter,
-    ));
-    let aggregation_job_driver = Arc::new(JobDriver::new(
-        clock,
-        TokioRuntime,
-        aggregation_job_driver_meter,
-        Duration::from_seconds(1),
-        Duration::from_seconds(2),
-        10,
-        Duration::from_seconds(1),
-        aggregation_job_driver.make_incomplete_job_acquirer_callback(
-            Arc::clone(&datastore),
-            Duration::from_seconds(10),
-        ),
-        aggregation_job_driver.make_job_stepper_callback(Arc::clone(&datastore), 3),
-    ));
-    let aggregation_job_driver_future = aggregation_job_driver.run();
-
-    // Run the collect job driver.
-    let collect_job_driver_meter = meter("collect_job_driver");
-    let collect_job_driver = Arc::new(CollectJobDriver::new(
-        reqwest::Client::new(),
-        &collect_job_driver_meter,
-    ));
-    let collect_job_driver = Arc::new(JobDriver::new(
-        clock,
-        TokioRuntime,
-        collect_job_driver_meter,
-        Duration::from_seconds(1),
-        Duration::from_seconds(2),
-        10,
-        Duration::from_seconds(1),
-        collect_job_driver.make_incomplete_job_acquirer_callback(
-            Arc::clone(&datastore),
-            Duration::from_seconds(10),
-        ),
-        collect_job_driver.make_job_stepper_callback(Arc::clone(&datastore), 3),
-    ));
-    let collect_job_driver_future = collect_job_driver.run();
-
-    tokio::spawn(aggregation_job_creator_future);
-    tokio::spawn(aggregation_job_driver_future);
-    tokio::spawn(collect_job_driver_future);
-
-    aggregator_future.await;
-
-    Ok(())
+        Ok(())
+    })
+    .await
 }
 
 #[cfg(test)]
 mod tests {
-    use super::app;
+    use super::Options;
+    use clap::CommandFactory;
 
     #[test]
     fn verify_clap_app() {
-        app().debug_assert();
+        Options::command().debug_assert();
     }
 }
