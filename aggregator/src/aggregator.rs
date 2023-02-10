cd52273529e4adaf79f5813eae321c573611cd63
David Cook <dcook@divviup.org>
2023-02-10 10:34:54 -0600

    Drop CollectReq bytes from tracing span

--- aggregator/src/aggregator.rs
+++ aggregator/src/aggregator.rs
@@ -2471,7 +2471,7 @@ impl VdafOps {
         }
     }
 
-    #[tracing::instrument(skip(datastore), err)]
+    #[tracing::instrument(skip(datastore, req_bytes), err)]
     async fn handle_collect_generic<
         const L: usize,
         Q: CollectableQueryType,
