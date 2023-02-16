18c780c3bca1bb934511bf2d4bb72fc8d1064461
David Cook <dcook@letsencrypt.org>
2023-02-16 15:30:51 -0600

    Add rerun-if command to janus_aggregator build.rs (#1021)

--- aggregator/build.rs
+++ aggregator/build.rs
@@ -3,4 +3,5 @@ use rustc_version::version;
 fn main() {
     let rustc_semver = version().expect("could not parse rustc version");
     println!("cargo:rustc-env=RUSTC_SEMVER={rustc_semver}");
+    println!("cargo:rerun-if-env-changed=RUSTC");
 }
