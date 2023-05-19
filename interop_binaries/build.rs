e94b4d648ac7a3424b150839c88c72421ba8beef
David Cook <dcook@divviup.org>
2023-05-19 14:00:44 -0500

    Use `docker buildx build` inside build script

--- interop_binaries/build.rs
+++ interop_binaries/build.rs
@@ -15,12 +15,13 @@ fn main() {
     #[cfg(feature = "testcontainer")]
     {
         use janus_build_script_utils::save_zstd_compressed_docker_image;
-        use std::{fs::File, process::Command};
+        use std::{fs::File, io::Read, process::Command};
 
         println!("cargo:rerun-if-env-changed=JANUS_INTEROP_CONTAINER");
         let container_strategy = env::var("JANUS_INTEROP_CONTAINER")
             .ok()
             .unwrap_or_else(|| "build".to_string());
+        let out_dir = env::var("OUT_DIR").unwrap();
 
         match container_strategy.as_str() {
             "build" => {
@@ -47,17 +48,24 @@ fn main() {
                 // any other image metadata (such as exposed ports, the entrypoint, etc), so we can't easily
                 // use it.
                 let client_image_id = {
+                    let image_id_file_name = format!("{out_dir}/janus_interop_client_image_id.txt");
+
                     let client_build_output = Command::new("docker")
                         .args([
+                            "buildx",
                             "build",
-                            "--quiet",
                             "--file=Dockerfile.interop",
                             "--build-arg=PROFILE=small",
                             "--build-arg=BINARY=janus_interop_client",
-                            ".",
+                            "--load",
                         ])
+                        .arg(format!("--iidfile={image_id_file_name}"))
+                        .arg(format!(
+                            "--cache-from=type=gha,scope={}-interop-small",
+                            env::var("GITHUB_REF_NAME").unwrap_or_else(|_| "main".to_string())
+                        ))
+                        .arg(".")
                         .current_dir("..")
-                        .env("DOCKER_BUILDKIT", "1")
                         .output()
                         .expect("Failed to execute `docker build` for interop client");
                     assert!(
@@ -65,23 +73,33 @@ fn main() {
                         "Docker build of interop client failed:\n{}",
                         String::from_utf8_lossy(&client_build_output.stderr)
                     );
-                    String::from_utf8(client_build_output.stdout)
-                        .unwrap()
-                        .trim()
-                        .to_string()
+                    let mut image_id = String::new();
+                    File::open(image_id_file_name)
+                        .expect("Failed to open image ID file")
+                        .read_to_string(&mut image_id)
+                        .expect("Failed to read image ID file");
+                    image_id.trim().to_string()
                 };
 
                 let aggregator_image_id = {
+                    let image_id_file_name =
+                        format!("{out_dir}/janus_interop_aggregator_image_id.txt");
+
                     let aggregator_build_output = Command::new("docker")
                         .args([
+                            "buildx",
                             "build",
-                            "--quiet",
                             "--file=Dockerfile.interop_aggregator",
                             "--build-arg=PROFILE=small",
-                            ".",
+                            "--load",
                         ])
+                        .arg(format!("--iidfile={image_id_file_name}"))
+                        .arg(format!(
+                            "--cache-from=type=gha,scope={}-interop-small",
+                            env::var("GITHUB_REF_NAME").unwrap_or_else(|_| "main".to_string())
+                        ))
+                        .arg(".")
                         .current_dir("..")
-                        .env("DOCKER_BUILDKIT", "1")
                         .output()
                         .expect("Failed to execute `docker build` for interop aggregator");
                     assert!(
@@ -89,24 +107,34 @@ fn main() {
                         "Docker build of interop aggregator failed:\n{}",
                         String::from_utf8_lossy(&aggregator_build_output.stderr)
                     );
-                    String::from_utf8(aggregator_build_output.stdout)
-                        .unwrap()
-                        .trim()
-                        .to_string()
+                    let mut image_id = String::new();
+                    File::open(image_id_file_name)
+                        .expect("Failed to open image ID file")
+                        .read_to_string(&mut image_id)
+                        .expect("Failed to read image ID file");
+                    image_id.trim().to_string()
                 };
 
                 let collector_image_id = {
+                    let image_id_file_name =
+                        format!("{out_dir}/janus_interop_collector_image_id.txt");
+
                     let collector_build_output = Command::new("docker")
                         .args([
+                            "buildx",
                             "build",
-                            "--quiet",
                             "--file=Dockerfile.interop",
                             "--build-arg=PROFILE=small",
                             "--build-arg=BINARY=janus_interop_collector",
-                            ".",
+                            "--load",
                         ])
+                        .arg(format!("--iidfile={image_id_file_name}"))
+                        .arg(format!(
+                            "--cache-from=type=gha,scope={}-interop-small",
+                            env::var("GITHUB_REF_NAME").unwrap_or_else(|_| "main".to_string())
+                        ))
+                        .arg(".")
                         .current_dir("..")
-                        .env("DOCKER_BUILDKIT", "1")
                         .output()
                         .expect("Failed to execute `docker build` for interop collector");
                     assert!(
@@ -114,40 +142,35 @@ fn main() {
                         "Docker build of interop collector failed:\n{}",
                         String::from_utf8_lossy(&collector_build_output.stderr)
                     );
-                    String::from_utf8(collector_build_output.stdout)
-                        .unwrap()
-                        .trim()
-                        .to_string()
+                    let mut image_id = String::new();
+                    File::open(image_id_file_name)
+                        .expect("Failed to open image ID file")
+                        .read_to_string(&mut image_id)
+                        .expect("Failed to read image ID file");
+                    image_id.trim().to_string()
                 };
 
                 // Save off containers to disk.
-                let client_image_file = File::create(format!(
-                    "{}/interop_client.tar.zst",
-                    env::var("OUT_DIR").unwrap()
-                ))
-                .expect("Couldn't create interop client image file");
+                let client_image_file = File::create(format!("{out_dir}/interop_client.tar.zst"))
+                    .expect("Couldn't create interop client image file");
                 save_zstd_compressed_docker_image(&client_image_id, &client_image_file);
                 client_image_file
                     .sync_all()
                     .expect("Couldn't write compressed image file");
                 drop(client_image_file);
 
-                let aggregator_image_file = File::create(format!(
-                    "{}/interop_aggregator.tar.zst",
-                    env::var("OUT_DIR").unwrap()
-                ))
-                .expect("Couldn't create interop aggregator image file");
+                let aggregator_image_file =
+                    File::create(format!("{out_dir}/interop_aggregator.tar.zst"))
+                        .expect("Couldn't create interop aggregator image file");
                 save_zstd_compressed_docker_image(&aggregator_image_id, &aggregator_image_file);
                 aggregator_image_file
                     .sync_all()
                     .expect("Couldn't write compressed image file");
                 drop(aggregator_image_file);
 
-                let collector_image_file = File::create(format!(
-                    "{}/interop_collector.tar.zst",
-                    env::var("OUT_DIR").unwrap()
-                ))
-                .expect("Couldn't create interop collector image file");
+                let collector_image_file =
+                    File::create(format!("{out_dir}/interop_collector.tar.zst"))
+                        .expect("Couldn't create interop collector image file");
                 save_zstd_compressed_docker_image(&collector_image_id, &collector_image_file);
                 collector_image_file
                     .sync_all()
@@ -175,11 +198,8 @@ fn main() {
                 // succeed; the consumer (testcontainer.rs) will panic if someone attempts to
                 // instantiate a container in this case.
                 for filename in ["interop_client", "interop_aggregator", "interop_collector"] {
-                    let image_file = File::create(format!(
-                        "{}/{filename}.tar.zst",
-                        env::var("OUT_DIR").unwrap()
-                    ))
-                    .expect("Couldn't create empty image file");
+                    let image_file = File::create(format!("{out_dir}/{filename}.tar.zst"))
+                        .expect("Couldn't create empty image file");
                     image_file
                         .sync_all()
                         .expect("Couldn't write empty image file");
