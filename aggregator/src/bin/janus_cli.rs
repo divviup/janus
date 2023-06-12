48d12ac09b716bb30aa9da203504934de7174a8b
David Cook <dcook@divviup.org>
2023-06-12 16:29:36 -0500

    janus_cli: Don't drop metrics exporter controller

--- aggregator/src/bin/janus_cli.rs
+++ aggregator/src/bin/janus_cli.rs
@@ -4,7 +4,7 @@ use clap::Parser;
 use janus_aggregator::{
     binary_utils::{database_pool, datastore, read_config, CommonBinaryOptions},
     config::{BinaryConfig, CommonConfig},
-    metrics::install_metrics_exporter,
+    metrics::{install_metrics_exporter, MetricsExporterHandle},
     trace::install_trace_subscriber,
 };
 use janus_aggregator_core::{
@@ -32,7 +32,8 @@ async fn main() -> Result<()> {
     let command_line_options = CommandLineOptions::parse();
     let config_file: ConfigFile = read_config(&command_line_options.common_options)?;
 
-    install_tracing_and_metrics_handlers(config_file.common_config()).await?;
+    let _metrics_handler =
+        install_tracing_and_metrics_handlers(config_file.common_config()).await?;
 
     debug!(?command_line_options, ?config_file, "Starting up");
 
@@ -135,14 +136,14 @@ impl Command {
     }
 }
 
-async fn install_tracing_and_metrics_handlers(config: &CommonConfig) -> Result<()> {
+async fn install_tracing_and_metrics_handlers(
+    config: &CommonConfig,
+) -> Result<MetricsExporterHandle> {
     install_trace_subscriber(&config.logging_config)
         .context("couldn't install tracing subscriber")?;
-    let _metrics_exporter = install_metrics_exporter(&config.metrics_config)
+    install_metrics_exporter(&config.metrics_config)
         .await
-        .context("failed to install metrics exporter")?;
-
-    Ok(())
+        .context("failed to install metrics exporter")
 }
 
 async fn provision_tasks<C: Clock>(
