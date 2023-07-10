7c993e448bcab20da098059abe7fab70ca6896cd
Ameer Ghani <inahga@divviup.org>
2023-07-10 11:52:56 -0400

    cargo fmt

--- aggregator/src/bin/janus_cli.rs
+++ aggregator/src/bin/janus_cli.rs
@@ -463,7 +463,7 @@ mod tests {
     use janus_aggregator::{
         binary_utils::CommonBinaryOptions,
         config::test_util::{generate_db_config, generate_metrics_config, generate_trace_config},
-        config::{CommonConfig},
+        config::CommonConfig,
     };
     use janus_aggregator_core::{
         datastore::{test_util::ephemeral_datastore, Datastore},
