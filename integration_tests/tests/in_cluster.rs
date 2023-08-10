0cee838c33dcba75c488a15262bb7d3b5328cc26
David Cook <dcook@divviup.org>
2023-08-10 18:38:18 -0500

    in_cluster: Create HPKE config in divviup-api first (#1693)
    * Update in_cluster test w/ new HPKE config resource
    
    * Bump version to 0.5.14

--- integration_tests/tests/in_cluster.rs
+++ integration_tests/tests/in_cluster.rs
@@ -13,7 +13,9 @@ use janus_core::{
 };
 use janus_integration_tests::{
     client::ClientBackend,
-    divviup_api_client::{DivviupApiClient, NewAggregatorRequest, NewTaskRequest},
+    divviup_api_client::{
+        DivviupApiClient, NewAggregatorRequest, NewHpkeConfigRequest, NewTaskRequest,
+    },
     TaskParameters,
 };
 use janus_messages::TaskId;
@@ -137,6 +139,16 @@ impl InClusterJanusPair {
             )
             .await;
 
+        let collector_hpke_config = divviup_api
+            .create_hpke_config(
+                &account,
+                &NewHpkeConfigRequest {
+                    name: "Integration test key".to_string(),
+                    contents: STANDARD.encode(task.collector_hpke_config().get_encoded()),
+                },
+            )
+            .await;
+
         let provision_task_request = NewTaskRequest {
             name: "Integration test task".to_string(),
             leader_aggregator_id: paired_leader_aggregator.id,
@@ -149,7 +161,7 @@ impl InClusterJanusPair {
             },
             expiration: "3000-01-01T00:00:00Z".to_owned(),
             time_precision_seconds: task.time_precision().as_seconds(),
-            hpke_config: STANDARD.encode(task.collector_hpke_config().get_encoded()),
+            hpke_config_id: collector_hpke_config.id,
         };
 
         // Provision the task into both aggregators via divviup-api
