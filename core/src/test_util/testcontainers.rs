417b84a0682dc2677689a96d978fb62aff1166fe
David Cook <dcook@divviup.org>
2023-08-29 15:57:41 -0500

    Replace lazy_static with std::sync::OnceLock

--- core/src/test_util/testcontainers.rs
+++ core/src/test_util/testcontainers.rs
@@ -1,17 +1,18 @@
 //! Testing functionality that interacts with the testcontainers library.
 
-use std::sync::{Arc, Mutex, Weak};
+use std::sync::{Arc, Mutex, OnceLock, Weak};
 use testcontainers::clients::Cli;
 
 /// Returns a container client, possibly shared with other callers to this function.
 pub fn container_client() -> Arc<Cli> {
-    // Once `Weak::new` is const in stable Rust, this should be replaced by a static variable
-    // initialized to `Mutex::new(Weak::new())`. [https://github.com/rust-lang/rust/issues/95091]
-    lazy_static::lazy_static! {
-        static ref CONTAINER_CLIENT_MU: Mutex<Weak<Cli>> = Mutex::new(Weak::new());
-    }
+    // Once `Weak::new` is const in stable Rust, in version 1.73, this should be replaced by a
+    // static variable initialized to `Mutex::new(Weak::new())`.
+    static CONTAINER_CLIENT_MU: OnceLock<Mutex<Weak<Cli>>> = OnceLock::new();
 
-    let mut container_client = CONTAINER_CLIENT_MU.lock().unwrap();
+    let mut container_client = CONTAINER_CLIENT_MU
+        .get_or_init(|| Mutex::new(Weak::new()))
+        .lock()
+        .unwrap();
     container_client.upgrade().unwrap_or_else(|| {
         let client = Arc::new(Cli::default());
         *container_client = Arc::downgrade(&client);
