e90f60bcc31026a1925f287c8fb0cb45bc50e2a9
David Cook <dcook@divviup.org>
2023-09-26 11:57:38 -0500

    Interop binaries: wait for database to be up

--- interop_binaries/setup.sh
+++ interop_binaries/setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+until pg_isready -U postgres; do sleep 1; done
 /usr/bin/supervisorctl -c /etc/janus/supervisord.conf start janus_interop_aggregator
 /usr/bin/supervisorctl -c /etc/janus/supervisord.conf start aggregation_job_creator
 /usr/bin/supervisorctl -c /etc/janus/supervisord.conf start aggregation_job_driver
