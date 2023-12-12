a1b177d02465e5cfd0829f97ea0ed64f44f1a918
Tim Geoghegan <timg@divviup.org>
2023-12-12 13:01:32 -0800

    Integration test for rate limits (#2334)
    Adds tests to exercise Divvi Up's rate limiting to the existing
    `in_cluster` test module. The test setup reads in rate limit
    configuration from a file whose path is specified via environment
    variable, akin to the existing `JANUS_E2E_*` variables.
    
    These tests don't actually exercise any part of Janus itself, but they
    rely on the test support for forwarding ports and otherwise working with
    a pair of Januses in a Kubernetes cluster, so this is a convenient place
    to define them.

--- integration_tests/tests/integration/in_cluster.rs
+++ integration_tests/tests/integration/in_cluster.rs
@@ -333,3 +333,264 @@ async fn in_cluster_fixed_size() {
     )
     .await;
 }
+
+#[cfg(feature = "in-cluster-rate-limits")]
+mod rate_limits {
+    use super::InClusterJanusPair;
+    use assert_matches::assert_matches;
+    use http::Method;
+    use janus_aggregator_core::task::QueryType;
+    use janus_core::{test_util::install_test_trace_subscriber, vdaf::VdafInstance};
+    use janus_messages::{AggregationJobId, CollectionJobId, TaskId};
+    use rand::random;
+    use reqwest::StatusCode;
+    use serde::Deserialize;
+    use std::{
+        env,
+        fs::File,
+        sync::{Arc, OnceLock},
+        time::Duration,
+    };
+    use tokio::sync::Semaphore;
+    use url::Url;
+
+    /// Configuration for the rate limit test. We need to know the QPS and the window over which
+    /// it is enforced so that we can send the appropriate number of requests. We load this config
+    /// from a file so that an integration test harness that knows what rate limits have been
+    /// deployed to the cluster we test against can set the values.
+    #[derive(Deserialize)]
+    struct TestConfig {
+        rate_limit_excess: f64,
+        window: u64,
+        upload_qps: u64,
+        aggregation_job_qps: u64,
+        collection_job_qps: u64,
+    }
+
+    impl TestConfig {
+        fn load() -> &'static Self {
+            static CONFIG: OnceLock<TestConfig> = OnceLock::new();
+
+            CONFIG.get_or_init(|| {
+                serde_json::from_reader(
+                    File::open(env::var("JANUS_E2E_RATE_LIMIT_TEST_CONFIG").unwrap()).unwrap(),
+                )
+                .unwrap()
+            })
+        }
+    }
+
+    struct RequestUrlParameters<'a> {
+        leader_url: &'a Url,
+        helper_url: &'a Url,
+        first_task_id: &'a TaskId,
+        second_task_id: &'a TaskId,
+    }
+
+    async fn run_rate_limit_test(
+        request_url_maker: &dyn Fn(&RequestUrlParameters) -> (Url, Url),
+        rate_limit_picker: &dyn Fn(&TestConfig) -> u64,
+        method: Method,
+    ) {
+        install_test_trace_subscriber();
+        let test_config = TestConfig::load();
+
+        let janus_pair =
+            InClusterJanusPair::new(VdafInstance::Prio3Count, QueryType::TimeInterval).await;
+
+        let (leader_url, helper_url) = janus_pair
+            .task_parameters
+            .endpoint_fragments
+            .endpoints_for_host_client(janus_pair.leader.port(), janus_pair.helper.port());
+        let other_task_id = random();
+
+        // Send requests to two different tasks, to prove that rate limits are per-task ID. It
+        // doesn't matter that one of the tasks doesn't exist for purposes of rate limits.
+        let (first_request_url, second_request_url) = request_url_maker(&RequestUrlParameters {
+            leader_url: &leader_url,
+            helper_url: &helper_url,
+            first_task_id: &janus_pair.task_parameters.task_id,
+            second_task_id: &other_task_id,
+        });
+        let rate_limit = rate_limit_picker(test_config);
+        let rate_limit_excess = test_config.rate_limit_excess;
+        let total_requests_count =
+            ((1.0 + rate_limit_excess) * (rate_limit * test_config.window) as f64) as u64;
+
+        let mut acceptable_status_count = 0;
+        let mut too_many_requests_count = 0;
+        let mut last_retry_after = None;
+        let client = reqwest::Client::builder().build().unwrap();
+
+        // Send ten requests at a time, because otherwise the kubectl port-forward gets overwhelmed
+        let semaphore = Arc::new(Semaphore::new(10));
+        let mut handles = Vec::new();
+        for _ in 0..total_requests_count {
+            for url in [first_request_url.clone(), second_request_url.clone()] {
+                let (client, method, semaphore) =
+                    (client.clone(), method.clone(), semaphore.clone());
+                handles.push(tokio::spawn(async move {
+                    let _permit = semaphore.acquire_owned().await.unwrap();
+                    // We avoid using janus_client here because we don't want it to automatically
+                    // retry on HTTP 429 for us.
+                    let response = client.request(method, url).send().await.unwrap();
+
+                    (
+                        response.headers().get("retry-after").cloned(),
+                        response.status(),
+                    )
+                }));
+            }
+        }
+
+        for handle in handles {
+            let (retry_after, status) = handle.await.unwrap();
+            // Every request this test send should get rejected due to a missing body if it gets
+            // past the rate limiter.
+            if status == StatusCode::BAD_REQUEST {
+                assert!(retry_after.is_none());
+                acceptable_status_count += 1
+            } else if status == StatusCode::TOO_MANY_REQUESTS {
+                assert_matches!(retry_after, Some(retry_after) => {
+                    let retry_after = retry_after.to_str().unwrap().parse::<u64>().unwrap();
+                    assert!(retry_after <= test_config.window);
+                    last_retry_after = Some(retry_after);
+                });
+                too_many_requests_count += 1
+            } else {
+                panic!("unexpected status {status:?}");
+            }
+        }
+
+        let ratio = too_many_requests_count as f64
+            / (acceptable_status_count + too_many_requests_count) as f64;
+        // We expect some exact percentage of requests to be rejected with HTTP 429, but allow a
+        // margin for error to account for cases where the test takes more than a second to run, or
+        // errors introduced by Caddy distributed rate limiting.
+        let expected_429_rate = rate_limit_excess / (1.0 + rate_limit_excess);
+        assert!(
+            ratio > expected_429_rate - 0.05 && ratio <= expected_429_rate + 0.05,
+            "ratio: {ratio} expected 429 rate: {expected_429_rate} \
+            count of HTTP 429: {too_many_requests_count} \
+            count of HTTP 400: {acceptable_status_count}",
+        );
+
+        let last_retry_after = assert_matches!(last_retry_after, Some(l) => l);
+
+        // Wait for prescribed time to elapse and then try again. Requests should go through.
+        std::thread::sleep(Duration::from_secs(last_retry_after));
+
+        for url in [first_request_url.clone(), second_request_url.clone()] {
+            let method = method.clone();
+            let response = client.request(method, url).send().await.unwrap();
+            assert_eq!(response.status(), StatusCode::BAD_REQUEST);
+            assert!(response.headers().get("retry-after").is_none());
+        }
+    }
+
+    #[tokio::test]
+    async fn upload() {
+        run_rate_limit_test(
+            &|RequestUrlParameters {
+                  leader_url,
+                  first_task_id,
+                  second_task_id,
+                  ..
+              }| {
+                (
+                    leader_url
+                        .join(&format!("tasks/{first_task_id}/reports"))
+                        .unwrap(),
+                    leader_url
+                        .join(&format!("tasks/{second_task_id}/reports"))
+                        .unwrap(),
+                )
+            },
+            &|test_config| test_config.upload_qps,
+            Method::PUT,
+        )
+        .await
+    }
+
+    #[rstest::rstest]
+    #[case::put(Method::PUT)]
+    #[case::post(Method::POST)]
+    #[case::delete(Method::DELETE)]
+    #[tokio::test]
+    async fn collection_job(#[case] method: Method) {
+        run_rate_limit_test(
+            &|RequestUrlParameters {
+                  leader_url,
+                  first_task_id,
+                  second_task_id,
+                  ..
+              }| {
+                let job_id: CollectionJobId = random();
+
+                (
+                    leader_url
+                        .join(&format!("tasks/{first_task_id}/collection_jobs/{job_id}"))
+                        .unwrap(),
+                    leader_url
+                        .join(&format!("tasks/{second_task_id}/collection_jobs/{job_id}"))
+                        .unwrap(),
+                )
+            },
+            &|test_config| test_config.collection_job_qps,
+            method,
+        )
+        .await
+    }
+
+    #[rstest::rstest]
+    #[case::put(Method::PUT)]
+    #[case::post(Method::POST)]
+    #[tokio::test]
+    async fn aggregation_job_put(#[case] method: Method) {
+        run_rate_limit_test(
+            &|RequestUrlParameters {
+                  helper_url,
+                  first_task_id,
+                  second_task_id,
+                  ..
+              }| {
+                let job_id: AggregationJobId = random();
+                (
+                    helper_url
+                        .join(&format!("tasks/{first_task_id}/aggregation_jobs/{job_id}"))
+                        .unwrap(),
+                    helper_url
+                        .join(&format!("tasks/{second_task_id}/aggregation_jobs/{job_id}"))
+                        .unwrap(),
+                )
+            },
+            &|test_config| test_config.aggregation_job_qps,
+            method,
+        )
+        .await
+    }
+
+    #[tokio::test]
+    async fn aggregate_share_post() {
+        run_rate_limit_test(
+            &|RequestUrlParameters {
+                  helper_url,
+                  first_task_id,
+                  second_task_id,
+                  ..
+              }| {
+                (
+                    helper_url
+                        .join(&format!("tasks/{first_task_id}/aggregate_shares"))
+                        .unwrap(),
+                    helper_url
+                        .join(&format!("tasks/{second_task_id}/aggregate_shares"))
+                        .unwrap(),
+                )
+            },
+            &|test_config| test_config.collection_job_qps,
+            Method::POST,
+        )
+        .await
+    }
+}
