9d856296478685e3b5d74a58c0ffaac18f5dd829
Ameer Ghani <inahga@divviup.org>
2024-01-22 13:46:56 -0500

    Fix for 0.6

--- aggregator/src/aggregator/aggregation_job_driver.rs
+++ aggregator/src/aggregator/aggregation_job_driver.rs
@@ -4147,13 +4147,7 @@ mod tests {
             .unwrap();
         let batch_identifier = TimeInterval::to_batch_identifier(&leader_task, &(), &time).unwrap();
         let report_metadata = ReportMetadata::new(random(), time);
-        let transcript = run_vdaf(
-            &vdaf,
-            verify_key.as_bytes(),
-            &(),
-            report_metadata.id(),
-            &false,
-        );
+        let transcript = run_vdaf(&vdaf, verify_key.as_bytes(), &(), report_metadata.id(), &0);
         let report = generate_report::<VERIFY_KEY_LENGTH, Prio3Count>(
             *task.id(),
             report_metadata,
