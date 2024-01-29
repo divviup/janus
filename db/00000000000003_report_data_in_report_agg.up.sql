2bb030b6977c7c6c649e8d7b660ff0b236b37589
Brandon Pitman <bran@bran.land>
2024-01-29 10:09:39 -0800

    Renumber DB migration to avoid conflict.

--- db/00000000000003_report_data_in_report_agg.up.sql
+++ db/00000000000003_report_data_in_report_agg.up.sql
@@ -2,4 +2,3 @@ ALTER TABLE report_aggregations ADD COLUMN public_share BYTEA;  -- the public sh
 ALTER TABLE report_aggregations ADD COLUMN leader_extensions BYTEA;  -- encoded sequence of Extension messages from Leader input share (opaque DAP messages)
 ALTER TABLE report_aggregations ADD COLUMN leader_input_share BYTEA;  -- encoded leader input share (opaque VDAF message)
 ALTER TABLE report_aggregations ADD COLUMN helper_encrypted_input_share BYTEA;  -- encoded HPKE ciphertext of helper input share (opaque DAP message)
-
