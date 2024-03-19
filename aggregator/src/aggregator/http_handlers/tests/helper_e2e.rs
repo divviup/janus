6f34f69148ec4e198cb9f2efbcafd4b6df5b94e6
David Cook <dcook@divviup.org>
2024-03-19 15:39:23 -0500

    Switch to fake VDAF

--- aggregator/src/aggregator/http_handlers/tests/helper_e2e.rs
+++ aggregator/src/aggregator/http_handlers/tests/helper_e2e.rs
@@ -7,7 +7,7 @@ use janus_messages::{
 };
 use prio::{
     codec::{Decode, Encode},
-    vdaf::prio3::Prio3,
+    vdaf::dummy,
 };
 use rand::random;
 use trillium::KnownHeaderName;
@@ -29,28 +29,22 @@ async fn helper_aggregation_report_share_replay() {
             max_batch_size: None,
             batch_time_window_size: None,
         },
-        VdafInstance::Prio3SumVec {
-            bits: 1,
-            length: 3,
-            chunk_length: 3,
-        },
+        VdafInstance::Fake { rounds: 1 },
     )
     .with_min_batch_size(1)
     .with_max_batch_query_count(1)
     .build();
-    let vdaf = Prio3::new_sum_vec(2, 1, 3, 3).unwrap();
+    let vdaf = dummy::Vdaf::new(1);
+    let agg_param = dummy::AggregationParam(0);
 
     let helper_task = task.helper_view().unwrap();
     datastore.put_aggregator_task(&helper_task).await.unwrap();
 
     let prep_init_generator =
-        PrepareInitGenerator::new(clock.clone(), helper_task.clone(), vdaf.clone(), ());
-    let (replayed_report, _replayed_report_transcript) =
-        prep_init_generator.next(&Vec::from([1, 0, 0]));
-    let (other_report_1, _other_report_1_transcript) =
-        prep_init_generator.next(&Vec::from([0, 1, 0]));
-    let (other_report_2, _other_report_2_transcript) =
-        prep_init_generator.next(&Vec::from([0, 0, 1]));
+        PrepareInitGenerator::new(clock.clone(), helper_task.clone(), vdaf.clone(), agg_param);
+    let (replayed_report, _replayed_report_transcript) = prep_init_generator.next(&7);
+    let (other_report_1, _other_report_1_transcript) = prep_init_generator.next(&11);
+    let (other_report_2, _other_report_2_transcript) = prep_init_generator.next(&23);
 
     let batch_id_1 = random();
     let batch_id_2 = random();
@@ -58,12 +52,12 @@ async fn helper_aggregation_report_share_replay() {
     let aggregation_job_id_2 = random();
 
     let agg_init_req_1 = AggregationJobInitializeReq::new(
-        Vec::new(),
+        agg_param.get_encoded().unwrap(),
         PartialBatchSelector::new_fixed_size(batch_id_1),
         Vec::from([replayed_report.clone(), other_report_1.clone()]),
     );
     let agg_init_req_2 = AggregationJobInitializeReq::new(
-        Vec::new(),
+        agg_param.get_encoded().unwrap(),
         PartialBatchSelector::new_fixed_size(batch_id_2),
         Vec::from([replayed_report.clone(), other_report_2.clone()]),
     );
@@ -73,14 +67,14 @@ async fn helper_aggregation_report_share_replay() {
             .updated_with(other_report_1.report_share().metadata().id());
     let agg_share_req_1 = AggregateShareReq::<FixedSize>::new(
         BatchSelector::new(batch_id_1),
-        Vec::new(),
+        agg_param.get_encoded().unwrap(),
         2,
         checksum_1,
     );
     let checksum_2 = ReportIdChecksum::for_report_id(other_report_2.report_share().metadata().id());
     let agg_share_req_2 = AggregateShareReq::<FixedSize>::new(
         BatchSelector::new(batch_id_2),
-        Vec::new(),
+        agg_param.get_encoded().unwrap(),
         1,
         checksum_2,
     );
