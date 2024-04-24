bc38c38d9d314a1289420c5e72caa61363af5dcb
David Cook <dcook@divviup.org>
2024-04-24 17:10:24 -0500

    Add deserializationt tests of metrics config

---
+++ aggregator/src/metrics/tests/config.rs
@@ -0,0 +1,53 @@
+use crate::metrics::{
+    MetricsConfiguration, MetricsExporterConfiguration, OtlpExporterConfiguration,
+};
+
+#[test]
+fn metrics_configuration_serde() {
+    let config = serde_yaml::from_str::<MetricsConfiguration>("---").unwrap();
+    assert_eq!(
+        config,
+        MetricsConfiguration {
+            exporter: None,
+            tokio: None
+        }
+    );
+
+    let config = serde_yaml::from_str::<MetricsConfiguration>(
+        "---
+exporter:
+  prometheus:
+    host: 0.0.0.0
+    port: 9464",
+    )
+    .unwrap();
+    assert_eq!(
+        config,
+        MetricsConfiguration {
+            exporter: Some(MetricsExporterConfiguration::Prometheus {
+                host: Some("0.0.0.0".into()),
+                port: Some(9464),
+            }),
+            tokio: None,
+        }
+    );
+
+    let config = serde_yaml::from_str::<MetricsConfiguration>(
+        "---
+exporter:
+  otlp:
+    endpoint: https://example.com/",
+    )
+    .unwrap();
+    assert_eq!(
+        config,
+        MetricsConfiguration {
+            exporter: Some(MetricsExporterConfiguration::Otlp(
+                OtlpExporterConfiguration {
+                    endpoint: "https://example.com/".into()
+                }
+            )),
+            tokio: None
+        }
+    );
+}
