3134aa25fb8d3e13f81e8a67e2193462e7a48fdd
David Cook <dcook@divviup.org>
2024-05-02 10:25:54 -0500

    Fix Clippy lints

--- core/src/test_util/dummy_vdaf.rs
+++ core/src/test_util/dummy_vdaf.rs
@@ -44,23 +44,17 @@ impl Vdaf {
         }
     }
 
-    pub fn with_prep_init_fn<F: Fn(&AggregationParam) -> Result<(), VdafError>>(
-        mut self,
-        f: F,
-    ) -> Self
+    pub fn with_prep_init_fn<F>(mut self, f: F) -> Self
     where
-        F: 'static + Send + Sync,
+        F: Fn(&AggregationParam) -> Result<(), VdafError> + Send + Sync + 'static,
     {
         self.prep_init_fn = Arc::new(f);
         self
     }
 
-    pub fn with_prep_step_fn<F: Fn() -> Result<PrepareTransition<Self, 0, 16>, VdafError>>(
-        mut self,
-        f: F,
-    ) -> Self
+    pub fn with_prep_step_fn<F>(mut self, f: F) -> Self
     where
-        F: 'static + Send + Sync,
+        F: Fn() -> Result<PrepareTransition<Self, 0, 16>, VdafError> + Send + Sync + 'static,
     {
         self.prep_step_fn = Arc::new(f);
         self
