597588433b926eb516977e8c71f33e147e941e7f
Ameer Ghani <inahga@divviup.org>
2024-07-15 17:34:15 -0500

    Retry on leader -> helper client errors

--- aggregator/src/aggregator/collection_job_driver.rs
+++ aggregator/src/aggregator/collection_job_driver.rs
@@ -18,7 +18,11 @@ use janus_aggregator_core::{
     },
     task,
 };
-use janus_core::{retries::is_retryable_http_status, time::Clock, vdaf_dispatch};
+use janus_core::{
+    retries::{is_retryable_http_client_error, is_retryable_http_status},
+    time::Clock,
+    vdaf_dispatch,
+};
 use janus_messages::{
     query_type::{FixedSize, QueryType, TimeInterval},
     AggregateShare, AggregateShareReq, BatchSelector,
@@ -631,6 +635,7 @@ where
             Error::Http(http_error_response) => {
                 is_retryable_http_status(http_error_response.status())
             }
+            Error::HttpClient(error) => is_retryable_http_client_error(error),
             Error::Datastore(error) => match error {
                 datastore::Error::Db(_) | datastore::Error::Pool(_) => true,
                 datastore::Error::User(error) => match error.downcast_ref::<Error>() {
