78ca119a3abe8d8af9cdfb5f12284f0cf835a708
David Cook <dcook@divviup.org>
2025-05-22 16:02:55 -0500

    Allow making smaller aggregation jobs after delay

--- integration_tests/src/janus.rs
+++ integration_tests/src/janus.rs
@@ -189,6 +189,7 @@ impl JanusInProcess {
             min_aggregation_job_size: 1,
             max_aggregation_job_size: 100,
             aggregation_job_creation_report_window: 5000,
+            late_report_grace_period_s: 3600,
         };
         let aggregation_job_driver_options = AggregationJobDriverOptions {
             common: common_binary_options.clone(),
