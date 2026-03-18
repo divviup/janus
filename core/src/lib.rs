373b0e48342bb9997b32352b3914d122ffb36f3b
Tim Geoghegan <timg@divviup.org>
2026-03-18 12:34:46 -0700

    update version tags for draft-ietf-ppm-dap-18 (#4473)
    - aggregator API version advertisement
    - domain separation tags for HPKE AAD
    - add footnotes to draft versions table in README.md
    
    Part of #4402

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -47,7 +47,7 @@ pub mod taskprov {
 
 /// This value is used in a few places throughout the protocol to identify the draft of DAP being
 /// implemented.
-const DAP_VERSION_IDENTIFIER: &str = "dap-16";
+const DAP_VERSION_IDENTIFIER: &str = "dap-18";
 
 /// Returns the given [`Url`], possibly modified to end with a slash.
 ///
